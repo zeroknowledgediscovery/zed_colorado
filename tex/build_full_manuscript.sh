9c0b6a4ee9353e8aae3f0d72b1f244b9ea4f9cf1
Ishanu Chattopadhyay <ishanuc@users.noreply.github.com>
2026-09-08 08:10:02 -0400

    Build single manuscript with integrated supplementary material

--- tex/build_full_manuscript.sh
+++ tex/build_full_manuscript.sh
@@ -9,11 +9,4 @@ latexmk \
   -halt-on-error \
   zebra_genomics_boundary_theorem_empirical.tex
 
-latexmk \
-  -pdf \
-  -interaction=nonstopmode \
-  -halt-on-error \
-  zebra_genomics_boundary_theorem_empirical_supplement.tex
-
 echo "Built: $(pwd)/zebra_genomics_boundary_theorem_empirical.pdf"
-echo "Built: $(pwd)/zebra_genomics_boundary_theorem_empirical_supplement.pdf"
