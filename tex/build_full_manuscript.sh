88672bf90d395e71983ddcd7af58fb8a5ef4946f
Ishanu Chattopadhyay <ishanuc@users.noreply.github.com>
2026-09-07 21:36:18 -0400

    Build canonical manuscript and computational supplement

--- tex/build_full_manuscript.sh
+++ tex/build_full_manuscript.sh
@@ -9,4 +9,11 @@ latexmk \
   -halt-on-error \
   zebra_genomics_boundary_theorem_empirical.tex
 
+latexmk \
+  -pdf \
+  -interaction=nonstopmode \
+  -halt-on-error \
+  zebra_genomics_boundary_theorem_empirical_supplement.tex
+
 echo "Built: $(pwd)/zebra_genomics_boundary_theorem_empirical.pdf"
+echo "Built: $(pwd)/zebra_genomics_boundary_theorem_empirical_supplement.pdf"
